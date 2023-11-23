dd12abed281ec74469e8f6852cb7aa2c3a64d8de
ReginaS1987 <reginasirazieva1987@gmail.com>
2023-11-23 12:37:51 +0300

    createVideoPlayer

--- Source/src/js/main.js
+++ Source/src/js/main.js
@@ -1,6 +1,10 @@
 import Slider from './modules/slider';
+import VideoPlayer from './modules/playVideo';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next');
     slider.render();
+
+    const palyer=new VideoPlayer('.showup .play', '.overlay') // в какой секции у нас кнопка. showup это секция
+    palyer.init();
 });
--- Source/src/js/modules/slider.js
+++ Source/src/js/modules/slider.js
@@ -13,29 +13,71 @@ export default class Slider {
 
         if (n < 1) {
             this.slideIndex = this.slides.length;
+
         }
+        try{
+            this.hanson.style.opacity='0';
+            if(n===3){
+                this.hanson.classList.add('animated');
+
+                setTimeout(() => {
+                    this.hanson.style.opacity='1';
+                    this.hanson.classList.add('slideInUp');
+
+
+
+  
+                },3000);
+            } else{
+                this.hanson.classList.remove('slideInUp');
+   
+            }
+        } catch (e){}
 
         Array.from(this.slides).forEach(slide => {
+          slide.classList.add('animated','fadeIn');
+
+
             slide.style.display = 'none';
         });
 
         this.slides[this.slideIndex - 1].style.display = 'block';
+
+
+
+
     }
 
     plusSlides(n) {
         this.showSlides(this.slideIndex += n);
+
+
+
+
+
     } 
 
     render() {
+try{
+    this.hanson=document.querySelector('.hanson');
+} catch{e}{}
+
         this.btns.forEach(item => {
             item.addEventListener('click', () => {
                 this.plusSlides(1);
+
+
+
+
+
             });
 
             item.parentNode.previousElementSibling.addEventListener('click', (e) => {
                 e.preventDefault();
                 this.slideIndex = 1;
                 this.showSlides(this.slideIndex);
+
+
             });
         });
 
